30c3db5f5f39dcf48fbf995364bb8684c47a6f84
Gabe Coelho <gabrielpalmacoelho@gmail.com>
2019-12-02 10:01:33 -0700

    changed some request attribute names

--- pm3/twitter-backend/src/lambdas/story/StoryRetriever.java
+++ pm3/twitter-backend/src/lambdas/story/StoryRetriever.java
@@ -4,6 +4,6 @@ import lambdas.dao.TweetDAO;
 
 public class StoryRetriever {
     public StoryResult handleRequest(StoryRequest storyRequest) {
-        return new TweetDAO().getStory(storyRequest.handle, storyRequest.itemSize, storyRequest.lastResult);
+        return new TweetDAO().getStory(storyRequest.handle, storyRequest.pageSize, storyRequest.lastResult);
     }
 }
